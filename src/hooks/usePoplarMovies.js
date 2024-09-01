9d37bd48405059095b4926573e6ae0061b11c36a
JiHun Kim <dydekffrl@naver.com>
2024-09-02 01:50:58 +0900

    usePopularMovies 한국어 설정 파라미터 추가

--- src/hooks/usePoplarMovies.js
+++ src/hooks/usePoplarMovies.js
@@ -2,7 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
 const fetchPopularMovies = () => {
-  return api.get(`/movie/popular`);
+  return api.get(`/movie/popular`, {
+    params: {
+      language: "ko-KR",
+    },
+  });
 };
 export const usePopularMoviesQuery = () => {
   return useQuery({
