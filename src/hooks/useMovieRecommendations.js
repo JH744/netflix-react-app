77461b868f80cbc0976ddcc9e184b1fa643b30c4
JiHun Kim <dydekffrl@naver.com>
2024-09-07 00:35:57 +0900

    style : 반응형 설정 추가

--- src/hooks/useMovieRecommendations.js
+++ src/hooks/useMovieRecommendations.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
 const fetchMovieRecommendations = ({ id }) => {
-  return api.get(`/movie/${id}/recommendations`);
+  return api.get(`/movie/${id}/recommendations?language=ko-KR`);
 };
 
 export const useMovieRecommendations = ({ id }) => {
