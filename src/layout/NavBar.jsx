5ed8110dd36821d0b61febf08d234803ffc4a872
JiHun Kim <dydekffrl@naver.com>
2024-09-08 04:30:50 +0900

    style : navbar의 메뉴 간격 조절

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -49,8 +49,8 @@ function NavBar() {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            className="me-auto my-2 my-lg-0 gap-1"
+            style={{ maxHeight: "100px", paddingLeft: "10px" }}
             navbarScroll
           >
             <Nav.Link
