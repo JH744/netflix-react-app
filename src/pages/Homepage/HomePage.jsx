017497ae627332738962881a0aebc90c0568da58
JiHun Kim <dydekffrl@naver.com>
2024-09-19 20:43:44 +0900

    style: opening logo 추가

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -4,11 +4,13 @@ import PopularMoviesSlide from "./components/PopularMoviesSlide/PopularMoviesSli
 import TopLatedMoviesSlide from "./components/TopLatedMoviesSlide/TopLatedMoviesSlide";
 import UpcomingMoviesSlide from "./components/UpcomingMoviesSlide/UpcomingMoviesSlide";
 import "./HomPage.style.css";
+import OpenLogo from "./components/OpenLogo";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <OpenLogo />
       <div className="slide-container">
         <PopularMoviesSlide />
         <TopLatedMoviesSlide />
